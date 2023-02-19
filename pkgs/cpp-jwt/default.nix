1d0332fad24593197e83116dece122819ed5a082
Nebojsa Koturovic <contact@kotur.me>
2023-02-19 21:07:26 +0100

    Add interpolation for  dir

--- pkgs/cpp-jwt/default.nix
+++ pkgs/cpp-jwt/default.nix
@@ -24,8 +24,8 @@ in stdenv.mkDerivation (finalAttrs: {
     "-DCPP_JWT_BUILD_EXAMPLES=OFF"
     "-DCPP_JWT_BUILD_TESTS=OFF"
     "-DCPP_JWT_USE_VENDORED_NLOHMANN_JSON=OFF"
-    "-DCMAKE_INSTALL_INCLUDEDIR=$out"
-    "-DCMAKE_INSTALL_DATADIR=$out"
+    "-DCMAKE_INSTALL_INCLUDEDIR=${out}"
+    "-DCMAKE_INSTALL_DATADIR=${out}"
   ];
 
   meta = with lib; {
