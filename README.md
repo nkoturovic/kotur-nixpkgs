69b63d760e1add424a3b7ee24e4e2374c34bbe6e
Nebojsa Koturovic <contact@kotur.me>
2023-02-24 22:32:23 +0100

    Pin nixpkgs version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+[![Built with nix](https://img.shields.io/static/v1?style=flat&logo=nixos&logoColor=white&label=&message=Built%20with%20Nix&color=41439a)](https://builtwithnix.org/)
+[![kotur.me](https://img.shields.io/badge/Author-kotur.me-blue?style=flat)](https://kotur.me)
+
 # kotur-nixpkgs
-Custom nixpkgs repository
+
+Custom nixpkgs (like) channel repository.
+
+- Project using the channel:
--- default.nix
+++ default.nix
@@ -1,10 +1,18 @@
-{ system ? builtins.currentSystem }:
-
-let
-   pkgs = import <nixpkgs> { inherit system; };
-
+{ system ? builtins.currentSystem,
+  pkgs ? let
+    nixpkgs = fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/50c23cd4ff6c8344e0b4d438b027b3afabfe58dd.tar.gz";
+      sha256 = "sha256-mNp9spRURoWWpoMKxoJ70DBJ6/rcL3QsS2ZbO9rHwpg=";
+    };
+  in
+    import nixpkgs {
+      overlays = [];
+      config = {};
+      inherit system;
+    },
+ }:
+ let
    callPackage = pkgs.lib.callPackageWith (pkgs // self); 
-
    self = { 
     cpp-jwt = callPackage ./pkgs/cpp-jwt { };
    }; 
