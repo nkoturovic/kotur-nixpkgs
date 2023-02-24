16f272f3e4691aef1fc9d5fdd6230627e4cc5987
Nebojsa Koturovic <contact@kotur.me>
2023-02-24 22:32:23 +0100

    Add default nixpkgs (pinned)

--- default.nix
+++ default.nix
@@ -1,10 +1,19 @@
-{ system ? builtins.currentSystem }:
+{ system ? builtins.currentSystem,
+  pkgs ? let
+    nixpkgs = fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/50c23cd4ff6c8344e0b4d438b027b3afabfe58dd.tar.gz";
+      sha256 = "sha256-+peLp16ruWLuTFHo0ZUbLlS1/meS/+RsWQQ9bUAzOh8=";
+    };
+  in
+    import nixpkgs {
+      overlays = [];
+      config = {};
+      inherit system;
+    },
+ }:
 
-let
-   pkgs = import <nixpkgs> { inherit system; };
-
+ let
    callPackage = pkgs.lib.callPackageWith (pkgs // self); 
-
    self = { 
     cpp-jwt = callPackage ./pkgs/cpp-jwt { };
    }; 
