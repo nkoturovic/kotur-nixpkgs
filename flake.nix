a7fc926ac3b82788b5ae571d5464036c31b6a942
Nebojsa Koturovic <contact@kotur.me>
2026-05-02 23:29:19 +0200

    Add whisper-cpp-vulkan via overlay in flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,42 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      kotur-pkgs = import ./default.nix {inherit system pkgs;};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            whisper-cpp-vulkan = prev.whisper-cpp.override {
+              vulkanSupport = true;
+              cudaSupport = false;
+              rocmSupport = false;
+              withFFmpegSupport = true;
+              withSDL = false;
+              ffmpeg = final.ffmpeg;
+              shaderc = final.shaderc;
+              vulkan-headers = final.vulkan-headers;
+              vulkan-loader = final.vulkan-loader;
+            };
+          })
+        ];
+      };
+      kotur-pkgs = import ./default.nix { inherit system pkgs; };
     in {
-      packages = kotur-pkgs;
+      packages = kotur-pkgs // { whisper-cpp-vulkan = pkgs.whisper-cpp-vulkan; };
+
+      # Overlay for consumers (nixos-dotfiles home-manager, etc.)
+      overlays.default = final: prev: {
+        whisper-cpp-vulkan = prev.whisper-cpp.override {
+          vulkanSupport = true;
+          cudaSupport = false;
+          rocmSupport = false;
+          withFFmpegSupport = true;
+          withSDL = false;
+          ffmpeg = final.ffmpeg;
+          shaderc = final.shaderc;
+          vulkan-headers = final.vulkan-headers;
+          vulkan-loader = final.vulkan-loader;
+        };
+      };
     });
 }
